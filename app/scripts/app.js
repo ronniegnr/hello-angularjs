6fe7873ea5cc3d972d87422f22574ddb9225104d
ronniegnr <gnr.ronnie@gmail.com>
2015-06-14 22:10:59 +0600

    new()

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,39 +1,46 @@
 'use strict';
 
+/**
+ * @ngdoc overview
+ * @name tryMeanApp
+ * @description
+ * # tryMeanApp
+ *
+ * Main module of the application.
+ */
 
-var app = angular.module('tryMeanApp', ['ui.router']);
+var app = angular.module('tryMeanApp', ['ngRoute']);
 
-app.config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state('main', {
-            url: '/',
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
             templateUrl: 'views/main.html',
             controller: 'MainCtrl'
         })
-        .state('about', {
-            url: '/about',
+        .when('/about', {
             templateUrl: 'views/about.html',
             controller: 'AboutCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
         });
-
-    $urlRouterProvider.otherwise('home');
 });
 
-
-app.factory('posts', [function () {
-    var object = {
-        posts: []
+app.factory('posts', [function(){
+    var o = {
+        posts: [
+            {title: 'post 1', upvotes: 5},
+            {title: 'post 2', upvotes: 4},
+            {title: 'post 3', upvotes: 2},
+            {title: 'post 4', upvotes: 1},
+            {title: 'post 5', upvotes: 5}
+        ]
     };
-    return object;
-}])
-
+    return o;
+}]);
 
 app.controller('TestCtrl', function($scope, posts) {
-
-    $scope.posts = posts.posts;
-
-    $scope.test = "test";
-
+    /*
     $scope.posts = [
         {title: 'post 1', upvotes: 5},
         {title: 'post 2', upvotes: 4},
@@ -41,6 +48,7 @@ app.controller('TestCtrl', function($scope, posts) {
         {title: 'post 4', upvotes: 1},
         {title: 'post 5', upvotes: 5}
     ];
+    */
 
     $scope.addPost = function() {
         if(!$scope.title || $scope.title==='') return;
