aee166bb2dceb1f1ff21fa52e83ca0efcf1b0a19
ronniegnr <gnr.ronnie@gmail.com>
2015-06-15 19:07:50 +0600

    hello angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,28 +1,32 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name helloAngularjsApp
- * @description
- * # helloAngularjsApp
- *
- * Main module of the application.
- */
-angular
-  .module('helloAngularjsApp', [
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
+var app = angular.module('helloAngularjsApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        .when('/', {
+            templateUrl: '/hellos.html',
+            controller: 'HelloController'
+        })
+        .when('/:id', {
+            templateUrl: '/helloDetails.html',
+            controller: 'HelloDetailsController'
+        })
+}]);
+
+app.factory('hellos', function () {
+    return [
+        {name: 'one', completed: true, note: "one note"},
+        {name: 'two', completed: true, note: "two note"},
+        {name: 'three', completed: false, note: "three note"},
+        {name: 'four', completed: true, note: "four note"}
+    ];
+});
+
+app.controller('HelloController', ['$scope', 'hellos', function ($scope, hellos) {
+    $scope.hellos = hellos;
+}]);
+
+app.controller('HelloDetailsController', ['$scope', '$routeParams', 'hellos', function($scope, $routeParams, hellos){
+    $scope.hello = hellos[$routeParams.id];
+}]);
